d634a83385f96820d12c30efe1cb95f6d68f051b
Vlad <7nomer7@gmail.com>
2025-04-28 17:48:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,41 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        print("Invalid command format.")
+        return
+
+    source_file = parts[1]
+    dest_file = parts[2]
+    if source_file == dest_file:
+        print("No action taken.")
+        return
+
+    try:
+        with (open(source_file, "r") as file_in,
+              open(dest_file, "w") as file_out):
+            file_out.write(file_in.read())
+        print(f"File '{source_file}' successfully copied to '{dest_file}'.")
+    except FileNotFoundError:
+        print(f"Error: File '{source_file}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    with open("file.txt", "w") as f:
+        f.write("This is the content of file.txt.\nIt has multiple lines.")
+
+    copy_file("cp file.txt file.txt")
+    copy_file("cp file.txt new_file.txt")
+    if os.path.exists("new_file.txt"):
+        with open("file.txt", "r") as f1, open("new_file.txt", "r") as f2:
+            print(
+                f"Content of file.txt equals new_file.txt: "
+                f"{f1.read() == f2.read()}"
+            )
+
+    copy_file("cp non_existent.txt error_file.txt")
+    copy_file("cp file.txt new_file.txt extra_argument")
